5822631bf4d75af60b8de05339e2a862641a894f
Xpynkuy <andrey_pgg@mail.ru>
2026-03-14 19:20:22 +0500

    create footer

---
+++ frontend/src/widgets/footer/ui/Footer.tsx
@@ -0,0 +1,8 @@
+import styles from "./Footer.module.scss";
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      © 2026 Language Platform by Xpynkuy - Начни изучать корейский прямо сейчас
+    </footer>
+  );
+};
