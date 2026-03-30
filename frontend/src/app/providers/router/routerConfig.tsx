730343bfba69351fed37222a1cadb4e9a2e1c0b7
Xpynkuy <andrey_pgg@mail.ru>
2026-03-30 16:38:01 +0500

    add new route

--- frontend/src/app/providers/router/routerConfig.tsx
+++ frontend/src/app/providers/router/routerConfig.tsx
@@ -2,6 +2,7 @@ import { AlphabetPage } from "@pages/alphabet";
 import { DictionaryPage } from "@pages/dictionary";
 import { ChatPage } from "@pages/gpt-chat";
 import { MainPage } from "@pages/main";
+import { NotFoundPage } from "@pages/not-found";
 import type { RouteProps } from "react-router-dom";
 
 export const AppRoutes = {
@@ -9,6 +10,7 @@ export const AppRoutes = {
   ALPHABET: "alphabet",
   DICTIONARY: "dictionary",
   CHAT: "chat",
+  NOT_FOUND: "not_found",
 } as const;
 
 export type AppRoutes = (typeof AppRoutes)[keyof typeof AppRoutes];
@@ -18,23 +20,29 @@ export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.ALPHABET]: "/alphabet",
   [AppRoutes.DICTIONARY]: "/dictionary",
   [AppRoutes.CHAT]: "/chat",
+  [AppRoutes.NOT_FOUND]: "*",
 };
 
 export const routeConfig: Record<AppRoutes, RouteProps> = {
   [AppRoutes.MAIN]: {
-    path: RoutePath.main,
+    path: RoutePath[AppRoutes.MAIN],
     element: <MainPage />,
   },
   [AppRoutes.ALPHABET]: {
-    path: RoutePath.alphabet,
+    path: RoutePath[AppRoutes.ALPHABET],
     element: <AlphabetPage />,
   },
   [AppRoutes.DICTIONARY]: {
-    path: RoutePath.dictionary,
+    path: RoutePath[AppRoutes.DICTIONARY],
     element: <DictionaryPage />,
   },
   [AppRoutes.CHAT]: {
-    path: RoutePath.chat,
+    path: RoutePath[AppRoutes.CHAT],
     element: <ChatPage />,
+  },
+
+  [AppRoutes.NOT_FOUND]: {
+    path: RoutePath[AppRoutes.NOT_FOUND],
+    element: <NotFoundPage />,
   },
 };
